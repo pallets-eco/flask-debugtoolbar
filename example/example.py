63a4eb022c1b09eccae10afd8e9f9a728d27e298
mvantellingen <michaelvantellingen@gmail.com>
2011-02-02 22:44:32 +0100

    Update example app

--- example/example.py
+++ example/example.py
@@ -1,14 +1,26 @@
+import sys
+sys.path.insert(0, '.')
+
 from flask import Flask, render_template
 from flaskext.script import Manager
+from flaskext.sqlalchemy import SQLAlchemy
 from flaskext.debugtoolbar import DebugToolbarExtension
 
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
+
 toolbar = DebugToolbarExtension(app)
+
+class ExampleModel(db.Model):
+    __tablename__ = 'examples'
+    value = db.Column(db.String(100), primary_key=True)
 
 @app.route('/')
 def index():
     app.logger.info("Hello there")
-
+    ExampleModel.query.get(1)
     return render_template('index.html')
 
 if __name__ == "__main__":
