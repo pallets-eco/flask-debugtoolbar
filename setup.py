dacc8ad09c43b74ee8cecea24f28b427ddae4ccc
mvantellingen <michaelvantellingen@gmail.com>
2011-02-05 21:45:47 +0100

    Update version number and package data

--- setup.py
+++ setup.py
@@ -3,17 +3,22 @@ from setuptools import setup, find_packages
 
 setup(
     name='Flask-DebugToolbar',
-    version='0.4.2',
+    version='0.4.4',
     url='http://github.com/mvantellingen/flask-debugtoolbar',
     license='BSD',
     author='Michael van Tellingen',
     author_email='michaelvantellingen@gmail.com',
     description='A port of the Django debug toolbar to Flask',
     long_description=__doc__,
-    packages=find_packages(),
     namespace_packages=['flaskext'],
     zip_safe=False,
     platforms='any',
+    packages=find_packages(exclude=['ez_setup']),
+    package_data={'flaskext.debugtoolbar': [
+        'static/css/*.css', 'static/js/*.js', 'static/img/*',
+        'templates/*.html', 'templates/panels/*.html'
+    ]},
+
     install_requires=[
         'setuptools',
         'Flask',
