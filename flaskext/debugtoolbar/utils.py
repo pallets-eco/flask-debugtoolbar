d7b98f9e77caef34cff9fc7de19ac4980abb9d38
mvantellingen <michaelvantellingen@gmail.com>
2011-02-13 21:19:15 +0100

    Add a format_fname function which translates a absolute filename to a filename relative to the sys.path or relative to the project root

---
+++ flaskext/debugtoolbar/utils.py
@@ -0,0 +1,35 @@
+import os.path
+import sys
+
+from flask import current_app
+
+def format_fname(value):
+    # If the value is not an absolute path, the it is a builtin or
+    # a relative file (thus a project file).
+    if not os.path.isabs(value):
+        if value.startswith(('{', '<')):
+            return value
+        if value.startswith('.' + os.path.sep):
+            return value
+        return '.' + os.path.sep + value
+
+    # If the file is absolute and within the project root handle it as
+    # a project file
+    if value.startswith(current_app.root_path):
+        return "." + value[len(current_app.root_path):]
+
+    # Loop through sys.path to find the longest match and return
+    # the relative path from there.
+    paths = sys.path
+    prefix = None
+    prefix_len = 0
+    for path in sys.path:
+        new_prefix = os.path.commonprefix([path, value])
+        if len(new_prefix) > prefix_len:
+            prefix = new_prefix
+            prefix_len = len(prefix)
+
+    if not prefix.endswith(os.path.sep):
+        prefix_len -= 1
+    path = value[prefix_len:]
+    return '<%s>' % path
