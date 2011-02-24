0f981a3d9afc593b4124360087ac76bdb726d0db
Michael van Tellingen <mvantellingen@macbookpro.local>
2011-02-24 21:11:57 +0100

    Add the sqlalchemy 'select' subpanel

--- flaskext/debugtoolbar/views.py
+++ flaskext/debugtoolbar/views.py
@@ -8,6 +8,34 @@ from flaskext.debugtoolbar.utils import format_sql
 
 module = Module(__name__)
 
+
+
+@module.route('/sql_select', methods=['GET', 'POST'])
+def sql_select(render):
+    statement = request.args['sql']
+    params = request.args['params']
+
+    # Validate hash
+    hash = hashlib.sha1(
+        current_app.config['SECRET_KEY'] + statement + params).hexdigest()
+    if hash != request.args['hash']:
+        return abort(406)
+
+    # Make sure it is a select statement
+    if not statement.lower().strip().startswith('select'):
+        return abort(406)
+
+    params = simplejson.loads(params)
+
+    db = SQLAlchemy(current_app)
+
+    result = db.engine.execute(statement, params).fetchall()
+    return render('panels/sqlalchemy_select.html', {
+        'result': result,
+        'sql': format_sql(statement, params),
+        'duration': float(request.args['duration']),
+    })
+
 @module.route('/sql_explain', methods=['GET', 'POST'])
 def sql_explain(render):
     statement = request.args['sql']
@@ -35,5 +63,5 @@ def sql_explain(render):
     return render('panels/sqlalchemy_explain.html', {
         'result': result,
         'sql': format_sql(statement, params),
-        'duration': request.args['duration'],
+        'duration': float(request.args['duration']),
     })
