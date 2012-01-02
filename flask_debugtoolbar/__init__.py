0e5fab943600173b8f5a814e67ca2eb1220aae75
Matt Good <matt@matt-good.net>
2012-01-02 13:03:30 -0800

    Restore calling process_view().
    Puts back the monkey-patch of dispatch_request() with fixes for calling
    self.foo() instead of app.foo() and uncommenting the process_view()
    call.

--- flask_debugtoolbar/__init__.py
+++ flask_debugtoolbar/__init__.py
@@ -48,6 +48,9 @@ class DebugToolbarExtension(object):
         self.app.before_request(self.process_request)
         self.app.after_request(self.process_response)
 
+        # Monkey-patch the Flask.dispatch_request method
+        app.dispatch_request = self.dispatch_request
+
         # Configure jinja for the internal templates and add url rules
         # for static data
         self.jinja_env = Environment(
@@ -60,6 +63,28 @@ class DebugToolbarExtension(object):
             '_debug_toolbar.static', self.send_static_file)
 
         app.register_blueprint(module, url_prefix='/_debug_toolbar/views')
+
+    def dispatch_request(self):
+        """Modified version of Flask.dispatch_request to call process_view."""
+        req = _request_ctx_stack.top.request
+        app = current_app
+
+        if req.routing_exception is not None:
+            app.raise_routing_exception(req)
+
+        rule = req.url_rule
+
+        # if we provide automatic options for this URL and the
+        # request came with the OPTIONS method, reply automatically
+        if getattr(rule, 'provide_automatic_options', False) \
+           and req.method == 'OPTIONS':
+            return app.make_default_options_response()
+
+        # otherwise dispatch to the handler for that endpoint
+        view_func = app.view_functions[rule.endpoint]
+        view_func = self.process_view(app, view_func, req.view_args)
+
+        return view_func(**req.view_args)
 
     def _show_toolbar(self):
         """Return a boolean to indicate if we need to show the toolbar."""
@@ -85,8 +110,7 @@ class DebugToolbarExtension(object):
 
     def process_view(self, app, view_func, view_kwargs):
         """ This method is called just before the flask view is called.
-
-        # FIXME/XXX: This method is currently not called!!
+        This is done by the dispatch_request method.
         """
         real_request = request._get_current_object()
         if real_request in self.debug_toolbars:
