d40208ab722e062365a2d76cd1de1635ad137488
Andrii Mishkovskyi <gdmfsob@mishkovskyi.net>
2012-04-04 15:46:54 +0200

    Emacs-specific setup

---
+++ .dir-locals.el
@@ -0,0 +1,3 @@
+((nil . ((virtualenv-workon . "flask-debugtoolbar")
+	 (virtualenv-default-directory . "~/nonwork/flask-debugtoolbar")))
+ (python-mode . ((compile-command . "nosetests -w ~/nonwork/flask-debugtoolbar"))))
