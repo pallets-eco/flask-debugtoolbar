83523d6f59e8d011d760e2fe1915bed2331d600d
Matt Good <matt@matt-good.net>
2012-05-09 15:18:55 -0700

    Update example.py to use flask.ext compatibility imports.

--- example/example.py
+++ example/example.py
@@ -2,8 +2,8 @@ import sys
 sys.path.insert(0, '.')
 
 from flask import Flask, render_template, redirect, url_for
-from flaskext.script import Manager
-from flaskext.sqlalchemy import SQLAlchemy
+from flask.ext.script import Manager
+from flask.ext.sqlalchemy import SQLAlchemy
 from flask_debugtoolbar import DebugToolbarExtension
 
 
