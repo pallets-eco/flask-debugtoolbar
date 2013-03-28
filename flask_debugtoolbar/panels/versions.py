093763909fa86cfdf841a1f18b2d5676ffc08ab3
Lucas Taylor <ltaylor.volks@gmail.com>
2013-03-27 17:16:28 -0700

    Display installed packages and versions in Versions panel (requires Yolk)

--- flask_debugtoolbar/panels/versions.py
+++ flask_debugtoolbar/panels/versions.py
@@ -1,3 +1,6 @@
+from distutils.sysconfig import get_python_lib
+import sys
+
 from flask import __version__ as flask_version
 from flask_debugtoolbar.panels import DebugPanel
 
@@ -8,7 +11,7 @@ class VersionDebugPanel(DebugPanel):
     Panel that displays the Flask version.
     """
     name = 'Version'
-    has_content = False
+    has_content = True
 
     def nav_title(self):
         return _('Versions')
@@ -23,6 +26,27 @@ class VersionDebugPanel(DebugPanel):
         return _('Versions')
 
     def content(self):
-        return None
+        try:
+            from yolk import yolklib
+        except ImportError:
+            "Requires yolk to provide package information"
+            context = self.context.copy()
+            context.update({
+                'packages': [],
+                'paths': sys.path
+                })
+        else:
+            dist = yolklib.Distributions()
+            active_packages = dist.get_packages('active')
+            _pkgs = dict([(p.project_name, p) for p in active_packages])
+            packages = [_pkgs[key] for key in sorted(_pkgs.iterkeys())]
+            for package in packages:
+                package.develop_mode = not (package.location.lower().startswith(get_python_lib().lower()))
 
+            context = self.context.copy()
+            context.update({
+                'packages': packages,
+                'paths': sys.path,
+            })
 
+        return self.render('panels/versions.html', context)
