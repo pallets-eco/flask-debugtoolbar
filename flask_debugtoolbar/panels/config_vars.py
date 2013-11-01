6d8249160dad574d935a54d42264a733c8003c5f
Matt Good <mgood@linkedin.com>
2013-11-01 14:04:10 -0700

    Rename "settings" panel to "config"

--- flask_debugtoolbar/panels/config_vars.py
+++ flask_debugtoolbar/panels/config_vars.py
@@ -3,18 +3,18 @@ from flask_debugtoolbar.panels import DebugPanel
 _ = lambda x: x
 
 
-class SettingsVarsDebugPanel(DebugPanel):
+class ConfigVarsDebugPanel(DebugPanel):
     """
     A panel to display all variables from Flask configuration
     """
-    name = 'SettingsVars'
+    name = 'ConfigVars'
     has_content = True
 
     def nav_title(self):
-        return _('Settings')
+        return _('Config')
 
     def title(self):
-        return _('Settings')
+        return _('Config')
 
     def url(self):
         return ''
@@ -22,7 +22,7 @@ class SettingsVarsDebugPanel(DebugPanel):
     def content(self):
         context = self.context.copy()
         context.update({
-            'settings': current_app.config,
+            'config': current_app.config,
         })
 
-        return self.render('panels/settings_vars.html', context)
+        return self.render('panels/config_vars.html', context)
