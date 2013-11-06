d5987410d106d2a22efab1307486c9ea71799264
Matt Good <matt@matt-good.net>
2013-11-06 09:00:05 -0800

    Add basic sanity test

---
+++ test/basic_app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask('basic_app')
+app.debug = True
+app.config['SECRET_KEY'] = 'abc123'
+
+# make sure these are printable in the config panel
+app.config['BYTES_VALUE'] = b'\x00'
+app.config['UNICODE_VALUE'] = u'\uffff'
+
+toolbar = DebugToolbarExtension(app)
+
+
+@app.route('/')
+def index():
+    return render_template('basic_app.html')
---
+++ test/test_toolbar.py
@@ -0,0 +1,11 @@
+def load_app(name):
+    app = __import__(name).app
+    app.config['TESTING'] = True
+    return app.test_client()
+
+
+def test_basic_app():
+    app = load_app('basic_app')
+    index = app.get('/')
+    assert index.status_code == 200
+    assert b'<div id="flDebug"' in index.data
