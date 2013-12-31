ffbdf0f56366dedc14cdbf8268d8ef907044969a
Matt Good <matt@matt-good.net>
2013-12-31 17:55:19 -0500

    Add SQLAlchemy to test app

--- test/basic_app.py
+++ test/basic_app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask_debugtoolbar import DebugToolbarExtension
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask('basic_app')
 app.debug = True
@@ -10,8 +11,20 @@ app.config['BYTES_VALUE'] = b'\x00'
 app.config['UNICODE_VALUE'] = u'\uffff'
 
 toolbar = DebugToolbarExtension(app)
+db = SQLAlchemy(app)
+
+
+class Foo(db.Model):
+    __tablename__ = 'foo'
+    id = db.Column(db.Integer, primary_key=True)
 
 
 @app.route('/')
 def index():
+    db.create_all()
+    Foo.query.filter_by(id=1).all()
     return render_template('basic_app.html')
+
+
+if __name__ == '__main__':
+    app.run()
