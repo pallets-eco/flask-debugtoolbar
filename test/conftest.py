d8780a65cd154914dcdfc99d355304c3d515a3d2
Nuno Santos <nunofvsantos@gmail.com>
2020-02-19 10:50:40 +0100

    Test fixes.

--- test/conftest.py
+++ test/conftest.py
@@ -16,40 +16,40 @@ def app_no_extensions(request):
     """ Flask app only, without any extensions. """
     _app = Flask(__name__)
 
-    app.debug = True
-    app.config["TESTING"] = True
-    app.config["SECRET_KEY"] = "abc123"
+    _app.debug = True
+    _app.config["TESTING"] = True
+    _app.config["SECRET_KEY"] = "abc123"
 
     # make sure these are printable in the config panel
-    app.config["BYTES_VALUE"] = b"\x00"
-    app.config["UNICODE_VALUE"] = u"\uffff"
+    _app.config["BYTES_VALUE"] = b"\x00"
+    _app.config["UNICODE_VALUE"] = u"\uffff"
 
     # config overrides
     if "config" in request.keywords:
         for key, value in request.keywords["config"].kwargs.items():
-            app.config[key] = value
+            _app.config[key] = value
 
     return _app
 
 
 @pytest.fixture
 def app(request, app_no_extensions):
-    app = app_no_extensions
+    _app = app_no_extensions
 
-    toolbar = DebugToolbarExtension(app)
-    db = SQLAlchemy(app)
+    toolbar = DebugToolbarExtension(_app)
+    db = SQLAlchemy(_app)
 
     class Foo(db.Model):
         __tablename__ = "foo"
         id = db.Column(db.Integer, primary_key=True)
 
-    @app.route("/")
+    @_app.route("/")
     def index():
         db.create_all()
         Foo.query.filter_by(id=1).all()
         return render_template("basic_app.html")
 
-    return app
+    return _app
 
 
 @pytest.fixture
