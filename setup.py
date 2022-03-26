8425a45205b047720cf9b8f917ea95f16885d5f5
Jeff Widman <jeff@jeffwidman.com>
2022-03-26 00:30:24 -0700

    Update PyPI metadata files: add `setup.cfg` etc
    Update to the newer PyPI / python packaging metadata file structure.
    
    A lot of this was cribbed from how [`Flask`](https://github.com/pallets/flask) itself
    exposes its metadata.

--- setup.py
+++ setup.py
@@ -1,49 +1,12 @@
-import os
 from setuptools import setup
 
-
-here = os.path.abspath(os.path.dirname(__file__))
-
-try:
-    README = open(os.path.join(here, 'README.rst')).read()
-    CHANGES = open(os.path.join(here, 'CHANGES.rst')).read()
-except Exception:
-    README = ''
-    CHANGES = ''
-
-
+# Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
 setup(
-    name='Flask-DebugToolbar',
-    version='0.12.dev0',
-    url='https://flask-debugtoolbar.readthedocs.io/',
-    license='BSD',
-    author='Michael van Tellingen',
-    author_email='michaelvantellingen@gmail.com',
-    maintainer='Matt Good',
-    maintainer_email='matt@matt-good.net',
-    description='A toolbar overlay for debugging Flask applications.',
-    long_description=README + '\n\n' + CHANGES,
-    zip_safe=False,
-    platforms='any',
-    include_package_data=True,
-    packages=[
-        'flask_debugtoolbar',
-        'flask_debugtoolbar.panels'
-    ],
+    name="Flask-DebugToolbar",
     install_requires=[
         'Flask>=0.8',
         'Blinker',
         'itsdangerous',
         'werkzeug',
     ],
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-        'Topic :: Software Development :: Libraries :: Python Modules'
-    ]
 )
