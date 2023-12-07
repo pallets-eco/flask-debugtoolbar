1c39a9ce47d95363c1f7aac8820789fda4a5a039
Jeff Widman <jeff@jeffwidman.com>
2023-12-06 17:22:06 -0800

    Require Flask >= `2.2.0`, Python >= `3.7`
    Require Flask >= `2.2.0`.
    
    I'm comfortable going up to requiring `3.x`, but when I grep'd for
    places we use older Flask constructs, this was all I found.
    
    So for now no need to jump further.
    
    Flask `2.2.0` requires Python >= `3.7`, so also dropped older pythons.

--- test/test_toolbar.py
+++ test/test_toolbar.py
@@ -1,7 +1,5 @@
 import sys
 
-import pytest
-
 from flask_debugtoolbar import _printable
 
 
@@ -16,26 +14,3 @@ def test_basic_app():
     index = app.get('/')
     assert index.status_code == 200
     assert b'<div id="flDebug"' in index.data
-
-
-@pytest.mark.skipif(sys.version_info >= (3,),
-                    reason='test only applies to Python 2')
-def test_printable_unicode():
-    class UnicodeRepr(object):
-        def __repr__(self):
-            return u'\uffff'
-
-    printable = _printable(UnicodeRepr())
-    assert "raised UnicodeEncodeError: 'ascii' codec" in printable
-
-
-@pytest.mark.skipif(sys.version_info >= (3,),
-                    reason='test only applies to Python 2')
-def test_printable_non_ascii():
-    class NonAsciiRepr(object):
-        def __repr__(self):
-            return 'a\xffb'
-
-    printable = u'%s' % _printable(NonAsciiRepr())
-    # should replace \xff with the unicode ? character
-    assert printable == u'a\ufffdb'
