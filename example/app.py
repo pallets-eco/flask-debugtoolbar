128bd5af6f74c6351f8e5c5c7f8c45444ab1421c
David Lord <davidism@gmail.com>
2024-04-29 10:48:24 -0700

    update project files
    use standard project layout and tool config across all pallets and
    pallets-eco projects
    
    * add issue templates
    * add dependabot grouped updates for actions and pypi
    * add lock inactive closed issues workflow
    * add publish workflow with slsa and trusted publishing
    * simplify tests workflow matrix
    * simplify docs config.py
    * show license in docs
    * use pip-compile to pin development dependencies
    * rename test to tests
    * add .editorconfig
    * simplify .gitignore
    * add pre-commit hooks (will run formatters  in a subsequent PR)
    * pin os and python in .readthedocs.yaml
    * update license with original commit date and pallets-eco
    * use pyproject.toml and flit_core instead of setuptools
    * only declare flask dependency
    * add config for mypy and pyright (will add typing in a subsequent PR)
    * readme is markdown
    * add pallets-eco message to readme
    * remove install and docs links from readme
    * add style, typing, docs tox envs
    * use faster wheel building tox config
    * add tox command to update dev dependencies

--- example/app.py
+++ example/app.py
@@ -1,21 +1,24 @@
 # Run using: `FLASK_DEBUG=True flask run`
 
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask
+from flask import redirect
+from flask import render_template
+from flask import url_for
 from flask_sqlalchemy import SQLAlchemy
+
 from flask_debugtoolbar import DebugToolbarExtension
 
-
 app = Flask(__name__)
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
-#app.config['DEBUG_TB_PANELS'] = (
+app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = True
+# app.config['DEBUG_TB_PANELS'] = (
 #    'flask_debugtoolbar.panels.headers.HeaderDebugPanel',
 #    'flask_debugtoolbar.panels.logger.LoggingPanel',
 #    'flask_debugtoolbar.panels.timer.TimerDebugPanel',
-#)
-#app.config['DEBUG_TB_HOSTS'] = ('127.0.0.1', '::1' )
-app.config['SECRET_KEY'] = 'asd'
-app.config['SQLALCHEMY_RECORD_QUERIES'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+# )
+# app.config['DEBUG_TB_HOSTS'] = ('127.0.0.1', '::1' )
+app.config["SECRET_KEY"] = "asd"
+app.config["SQLALCHEMY_RECORD_QUERIES"] = True
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/test.db"
 # This is no longer needed for Flask-SQLAlchemy 3.0+, if you're using 2.X you'll want to define this:
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -24,21 +27,21 @@ toolbar = DebugToolbarExtension(app)
 
 
 class ExampleModel(db.Model):
-    __tablename__ = 'examples'
+    __tablename__ = "examples"
     value = db.Column(db.String(100), primary_key=True)
 
 
-@app.route('/')
+@app.route("/")
 def index():
     app.logger.info("Hello there")
     ExampleModel.query.get(1)
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route('/redirect')
+@app.route("/redirect")
 def redirect_example():
-    response = redirect(url_for('index'))
-    response.set_cookie('test_cookie', '1')
+    response = redirect(url_for("index"))
+    response.set_cookie("test_cookie", "1")
     return response
 
 
